c9cda7f980c9a3c2693f0ee42edaab7b629e4d49
haneesh byreddy <haneeshbyreddy@gmail.com>
2024-08-13 14:28:40 +0530

    added n_queen solution

---
+++ Python Code/Practice/Algorithms/n_queen.py
@@ -0,0 +1,35 @@
+import copy
+
+n = 8
+matrix = [[0]*n for _ in range(n)]
+ans_arr = [0]*n
+
+def mark(row, col, input_matrix):
+    for i in range(n):
+        input_matrix[row][i] = 1
+        input_matrix[i][col] = 1
+
+    for i in range(n):
+        if 0 <= row + i < n and 0 <= col + i < n:
+            input_matrix[row + i][col + i] = 1
+        if 0 <= row - i < n and 0 <= col - i < n:
+            input_matrix[row - i][col - i] = 1
+        if 0 <= row + i < n and 0 <= col - i < n:
+            input_matrix[row + i][col - i] = 1
+        if 0 <= row - i < n and 0 <= col + i < n:
+            input_matrix[row - i][col + i] = 1
+
+def n_queen(row, matrix):
+    if row>=len(matrix):
+        return True
+    for i in range(len(matrix[0])):
+        ans_arr[row] = i
+        if matrix[row][i] == 0:
+            temp = copy.deepcopy(matrix)
+            mark(row, i, temp)
+            if n_queen(row+1, temp):
+                return True
+    return False 
+
+print(n_queen(0, matrix))
+print(ans_arr)

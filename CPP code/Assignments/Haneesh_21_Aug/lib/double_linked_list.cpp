d27ffff423737724183265460b5bfab79c39c003
haneesh byreddy <haneeshbyreddy@gmail.com>
2024-08-21 20:54:25 +0530

    Haneesh_21_Aug_CPP-1

--- CPP code/Assignments/Haneesh_21_Aug/lib/double_linked_list.cpp
+++ CPP code/Assignments/Haneesh_21_Aug/lib/double_linked_list.cpp
@@ -85,10 +85,7 @@ int DoubleLinkedList::Multiply(const DoubleLinkedList& dup) {
 }
 
 DoubleLinkedList DoubleLinkedList::QuickSort(bool inc) {
-    if (head == nullptr || head == tail) {
-        return *this;
-    }
-
+    if (head == nullptr || head == tail) return *this;
     DoubleLinkedList l1, l2;
     DoubleLinkedNode* ref = new DoubleLinkedNode;
     ref->value = tail->value;
@@ -108,21 +105,55 @@ DoubleLinkedList DoubleLinkedList::QuickSort(bool inc) {
 
     if (!inc) swap(l1, l2);
 
-    if (l1.head != nullptr) {
-        l1.tail->next = ref;
-        ref->prev = l1.tail;
-        l1.tail = ref;
-    } else {
-        l1.head = l1.tail = ref;
-    }
-
-    if (l2.head != nullptr) {
+    if (l1.tail) l1.tail->next=ref, ref->prev=l1.tail;
+    else l1.head = ref;
+    l1.tail = ref;
+    if (l2.head) {
         ref->next = l2.head;
         l2.head->prev = ref;
         l1.tail = l2.tail;
-    } else {
-        l1.tail = ref;
     }
 
     return l1;
+}
+
+DoubleLinkedList DoubleLinkedList::MergeSort() {
+    if (head == nullptr || head == tail) return *this;
+
+    DoubleLinkedNode* slow = head;
+    DoubleLinkedNode* fast = head;
+
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    DoubleLinkedList l1, l2;
+    DoubleLinkedNode* mid = slow;
+    DoubleLinkedNode* curr = head;
+
+    while (curr != mid) l1.Push(curr->value), curr=curr->next;
+    while (mid) l2.Push(mid->value), mid=mid->next;
+
+    l1 = l1.MergeSort();
+    l2 = l2.MergeSort();
+
+    DoubleLinkedList ans;
+    DoubleLinkedNode* left = l1.head;
+    DoubleLinkedNode* right = l2.head;
+
+    while (left && right) {
+        if (left->value < right->value) {
+            ans.Push(left->value);
+            left = left->next;
+        } else {
+            ans.Push(right->value);
+            right = right->next;
+        }
+    }
+
+    while (left) ans.Push(left->value), left=left->next;
+    while (right) ans.Push(right->value), right=right->next;
+
+    return ans;
 }
